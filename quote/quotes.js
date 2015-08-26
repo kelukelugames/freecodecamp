a4251be285bea045264d9d07669a842da0e369d2
kelukelugames <kelukelugames@gmail.com>
2015-08-25 19:05:09 -0700

    Better variables

--- quote/quotes.js
+++ quote/quotes.js
@@ -1,15 +1,19 @@
 
 $(document).ready(function() {
-	quoteArray = [
-	"You are on the cusp of being a senior developer. We will promote you in no time.",
+	recruiterQuotes = [
+	"You are very close to becoming a senior developer. We will promote you in no time.",
 	"We don't negotiate.",
 	"I can't answer that because of legal reasons.",
-	"The entire team is made of rock star devs",
+	"We only hire rock star devs",
 	"I'm hiring and thought that you would be a good fit for the position that I'm trying to fill - details below. Let me know whether you're interested in discussing this opportunity. If you aren't but know someone who is, I'd love a referral!"];
 
-	document.getElementById("quote").innerHTML = quoteArray[Math.floor(Math.random() * quoteArray.length)];
+	setQuote();
 
 	$("button").on("click", function() {
-		document.getElementById("quote").innerHTML = quoteArray[Math.floor(Math.random() * quoteArray.length)];
+		setQuote();
 	});
+
+	function setQuote() {
+		document.getElementById("quote").innerHTML = recruiterQuotes[Math.floor(Math.random() * recruiterQuotes.length)];
+	}
 });
