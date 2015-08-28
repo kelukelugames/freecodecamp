46a415dad25777da4c4f7ffdea2fdcea69ec05dd
kelukelugames <kelukelugames@gmail.com>
2015-08-27 19:55:12 -0700

    Nailed it!

--- weather/script.js
+++ weather/script.js
@@ -16,14 +16,10 @@ function getWeather(location) {
     lat = location.split(',')[0];
     lon = location.split(',')[1];
     var weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=' + "imperial";
-    console.log(weatherApiUrl);
     $.get(weatherApiUrl, function (weather) {
         var temperature = Math.round(weather.main.temp);
-        console.log(weather);
-
-            $(".temperature").append(temperature + " F");
-            $(".weather-icon").append("<img src='http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png'>");
-            
-            $(".condition").append(weather.weather[0].description);
+        $(".temperature").append(temperature + " F");
+        $(".weather-icon").append("<img src='http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png'>");
+        $(".condition").append(weather.weather[0].description);
         }, 'jsonp');
 }
