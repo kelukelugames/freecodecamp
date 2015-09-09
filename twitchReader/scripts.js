605ac9142a635369517fc6237e2e03286ef5e0de
kelukelu <kelu.eecs@gmail.com>
2015-09-08 19:00:20 -0700

    Add an arrow that moves

--- twitchReader/scripts.js
+++ twitchReader/scripts.js
@@ -1,1 +1,9 @@
-console.warn("poop")
+
+      var app = angular.module('TwitchReader', []);
+app.controller('SelectionController', function () {
+
+    $('.menu li').on('click', function () {
+        $('.menu li').removeClass('active');
+        $(this).addClass('active');
+    });
+});
